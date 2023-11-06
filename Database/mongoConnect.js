09d137572d1b7803db9f0b7a42d064f099680057
yanos1 <yanos241999@gmail.com>
2023-11-06 13:34:12 +0200

    testing db

--- Database/mongoConnect.js
+++ Database/mongoConnect.js
@@ -6,7 +6,7 @@ main().catch(err => console.log(err));
 
 async function main() {
     try {
-        await mongoose.connect(`mongodb+srv://${config.DBUSERNAME}:${config.DBPASSWORD}@cluster0.jktqllf.mongodb.net/PostsDatabase`)
+        await mongoose.connect(`mongodb+srv://${config.DBUSERNAME}:${config.DBPASSWORD}@cluster0.jktqllf.mongodb.net`)  //removed /PostsDatabase
     }
     catch (err) {
         console.log("ERROR")
